6b6dd51fa00eb9878e4f2873c23eb5671becf4a7
S312User <S312User@S312.uc-bcf.edu.ph>
2024-03-08 12:26:37 +0800

    Renamed a file

--- style.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var myForm = document.getElementById('myForm');
-
-    myForm.addEventListener('submit', function(event) {
-event.preventDefault();
-alert("Form submitted successfully")
-});
-});
